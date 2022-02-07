047f6275b48cc1edcec1075cd456677d90ea0434
Youngsung Kim <kimy@ornl.gov>
2022-02-07 16:05:28 -0500

    minor update in consts

--- accelpy/const.py
+++ accelpy/const.py
@@ -7,7 +7,15 @@ long_description= "Scalable Accelerator Interface in Python"
 author          = "Youngsung Kim"
 vendors         = ("cray", "amd", "nvidia", "intel", "pgi", "ibm", "gnu")
 
-NOCACHE, FILECACHE, MEMCACHE = range(3)
-NODEBUG, MINDEBUG, MAXDEBUG  = range(3)
-NOPROF, MINPROF, MAXPROF  = range(3)
+NOCACHE         = 0
+FILECACHE       = 1
+MEMCACHE        = 2 
+
+NODEBUG         = 0
+MINDEBUG        = 1
+MAXDEBUG        = 2
+
+NOPROF          = 0
+MINPROF         = 1
+MAXPROF         = 2
 
