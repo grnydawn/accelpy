dcb3b3e9cfc7b4541a89df00d1937efda4101253
Youngsung Kim <kimy@ornl.gov>
2022-03-24 15:54:13 -0400

    ver 0.6.1

--- accelpy/const.py
+++ accelpy/const.py
@@ -1,7 +1,7 @@
 "Constant module"
 
 name            = "accelpy"
-version         = "0.6.0"
+version         = "0.6.1"
 description     = "Scalable Accelerator Interface in Python"
 long_description= "Scalable Accelerator Interface in Python"
 author          = "Youngsung Kim"
